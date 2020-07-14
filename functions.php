40dd3473441b69744e0cbf4b3d5e6fd1a5ba4b7d
dannyhuang <kuanhsuh@gmail.com>
2020-07-14 15:18:28 +0800

    Finish Single

--- functions.php
+++ functions.php
@@ -186,6 +186,30 @@ function hiskio_agency_scripts()
 add_action('wp_enqueue_scripts', 'hiskio_agency_scripts');
 
 /**
+ * Filter the except length to 20 words.
+ *
+ * @param int $length Excerpt length.
+ * @return int (Maybe) modified excerpt length.
+ */
+function wpdocs_custom_excerpt_length($length)
+{
+  return 200;
+}
+add_filter('excerpt_length', 'wpdocs_custom_excerpt_length', 999);
+
+/**
+ * Filter the excerpt "read more" string.
+ *
+ * @param string $more "Read more" excerpt string.
+ * @return string (Maybe) modified "read more" excerpt string.
+ */
+function wpdocs_excerpt_more($more)
+{
+  return '...';
+}
+add_filter('excerpt_more', 'wpdocs_excerpt_more');
+
+/**
  * Implement the Custom Header feature.
  */
 require get_template_directory() . '/inc/custom-header.php';
