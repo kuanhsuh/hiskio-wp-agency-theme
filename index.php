157065e24113b0b5e70ca60f69baae8e73321ff1
dannyhuang <kuanhsuh@gmail.com>
2020-07-17 14:14:33 +0800

    Blog index pagination & sidebar

--- index.php
+++ index.php
@@ -78,64 +78,13 @@ get_header();
         </div>
       </div>
 
-      <div class="col-lg-4 col-md-4">
-        <div class="sidebar-side">
-          <!-- Search sidebar-->
-          <div class="sidebar-search">
-            <form class="search clearfix" method="get" action="#">
-              <input type="text" name="name-filed" placeholder="Search ou blog" />
-              <button type="submit"><i class="icon_search"></i></button>
-            </form>
-          </div>
-
-          <!-- Search sidebar-->
-
-          <div class="sidebar-categroies">
-            <!-- Sidebar categories-->
-            <h3>Categories</h3>
-
-            <ul class="catgeorie-list">
-              <li>
-                <a href="" class="clearfix"> <span>Web design</span> </a>
-              </li>
-
-              <li>
-                <a href="" class="clearfix">
-                  <span>Developpement</span>
-                </a>
-              </li>
-
-              <li>
-                <a href="" class="clearfix"> <span>UX design</span> </a>
-              </li>
-
-              <li>
-                <a href="" class="clearfix"> <span>Others</span> </a>
-              </li>
-            </ul>
-            <!-- Sidebar categories-->
-          </div>
-
-          <div class="sidebar-tags">
-            <!-- Tags sidebar-->
-
-            <h3>Tags</h3>
-
-            <div class="tags-inner">
-              <a href="">Web Design</a>
-              <a href="">Inspiration</a>
-              <a href="">UX Design</a>
-              <a href="">Animation</a>
-              <a href="">Developpement</a>
-              <a href="">UI Design</a>
-              <a href="">Mobile</a>
-            </div>
-            <!-- Tags sidebar-->
-          </div>
-        </div>
-      </div>
+      <?php get_sidebar(); ?>
+    </div>
+    <div class="mt-4">
+      <?php wp_pagenavi(); ?>
     </div>
   </div>
+
 </section>
 
 <?php
--- sidebar.php
+++ sidebar.php
@@ -1,17 +1,56 @@
-<?php
-/**
- * The sidebar containing the main widget area
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package hiskio-agency
- */
+<div class="col-lg-4 col-md-4">
+  <div class="sidebar-side">
+    <!-- Search sidebar-->
+    <div class="sidebar-search">
+      <form class="search clearfix" method="get" action="#">
+        <input type="text" name="name-filed" placeholder="Search ou blog" />
+        <button type="submit"><i class="icon_search"></i></button>
+      </form>
+    </div>
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
-?>
+    <!-- Search sidebar-->
 
-<aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</aside><!-- #secondary -->
+    <div class="sidebar-categroies">
+      <!-- Sidebar categories-->
+      <h3>Categories</h3>
+
+      <ul class="catgeorie-list">
+        <li>
+          <a href="" class="clearfix"> <span>Web design</span> </a>
+        </li>
+
+        <li>
+          <a href="" class="clearfix">
+            <span>Developpement</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="" class="clearfix"> <span>UX design</span> </a>
+        </li>
+
+        <li>
+          <a href="" class="clearfix"> <span>Others</span> </a>
+        </li>
+      </ul>
+      <!-- Sidebar categories-->
+    </div>
+
+    <div class="sidebar-tags">
+      <!-- Tags sidebar-->
+
+      <h3>Tags</h3>
+
+      <div class="tags-inner">
+        <a href="">Web Design</a>
+        <a href="">Inspiration</a>
+        <a href="">UX Design</a>
+        <a href="">Animation</a>
+        <a href="">Developpement</a>
+        <a href="">UI Design</a>
+        <a href="">Mobile</a>
+      </div>
+      <!-- Tags sidebar-->
+    </div>
+  </div>
+</div>
